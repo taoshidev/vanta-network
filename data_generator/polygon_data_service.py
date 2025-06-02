857a1fe20108df72791f6b3171d4882f8c831758
Gordon Kim <gordonkimor@gmail.com>
2025-06-02 12:05:09 -0700

    Fix lint error for polygon rest case

--- data_generator/polygon_data_service.py
+++ data_generator/polygon_data_service.py
@@ -931,7 +931,8 @@ if __name__ == "__main__":
         #if tp != TradePair.GBPUSD:
         #    continue
 
-        print('getting close for', tp.trade_pair_id, ':', polygon_data_provider.get_close_rest(tp))
+        print('PRICE BEFORE MARKET CLOSE: ', polygon_data_provider.get_event_before_market_close(tp))
+        print('getting close for', tp.trade_pair_id, ':', polygon_data_provider.get_close_rest(tp, TimeUtil.now_in_millis()))
 
     time.sleep(100000)
 
