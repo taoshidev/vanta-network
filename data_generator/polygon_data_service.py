a3fae3e037e3883f15c9ca87356449ca03e3c594
Gordon Kim <gordonkimor@gmail.com>
2025-06-02 11:36:14 -0700

    Pass closest timestamp to data services with live flag

--- data_generator/polygon_data_service.py
+++ data_generator/polygon_data_service.py
@@ -392,7 +392,7 @@ class PolygonDataService(BaseDataService):
             raise ValueError(f"Unknown symbol: {symbol}")
         return tp
 
-    def get_closes_rest(self, trade_pairs: List[TradePair], time_ms) -> dict:
+    def get_closes_rest(self, trade_pairs: List[TradePair], time_ms, live=True) -> dict:
         all_trade_pair_closes = {}
         # Multi-threaded fetching of REST data over all requested trade pairs. Max parallelism is 5.
         with ThreadPoolExecutor(max_workers=5) as executor:
@@ -456,7 +456,7 @@ class PolygonDataService(BaseDataService):
     def get_close_rest(
         self,
         trade_pair: TradePair,
-        timestamp_ms: int = None,
+        timestamp_ms: int,
         order: Order = None
     ) -> PriceSource | None:
         if not timestamp_ms:
@@ -466,8 +466,8 @@ class PolygonDataService(BaseDataService):
             if order is not None and order.src == 0:
                 assert self.is_market_open(trade_pair, time_ms=timestamp_ms)
 
-        if not self.is_market_open(trade_pair, time_ms=timestamp_ms):
-            return self.get_event_before_market_close(trade_pair, timestamp_ms)
+        if not self.is_market_open(trade_pair):
+            return self.get_event_before_market_close(trade_pair)
 
         prev_timestamp = None
         final_agg = None
