f0e5874d5e3370c1cdba7f6cfa19c6013a7366ca
ward-taoshi <236940013+ward-taoshi@users.noreply.github.com>
2026-02-02 18:42:32 -0500

    Push autosync time

--- vali_objects/data_sync/auto_sync.py
+++ vali_objects/data_sync/auto_sync.py
@@ -137,7 +137,7 @@ class PositionSyncer(ValidatorSyncBase):
             return
 
         datetime_now = TimeUtil.generate_start_timestamp(0)  # UTC
-        if not (datetime_now.hour == 21 and (7 < datetime_now.minute < 17)):
+        if not (datetime_now.hour == 0 and (7 < datetime_now.minute < 17)):
             return
 
         self.perform_sync()
