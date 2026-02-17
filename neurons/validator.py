f9170075570933d97e1b21e6f6c924a12f6add7b
Michael Brown <mrmichaelrb@gmail.com>
2026-02-17 23:53:44 +0800

    Prevent all endpoints from blocking event loop

--- neurons/validator.py
+++ neurons/validator.py
@@ -685,7 +685,7 @@ class Validator(ValidatorBase):
 
         return synapse
 
-    def get_positions(self, synapse: template.protocol.GetPositions,
+    def _get_positions(self, synapse: template.protocol.GetPositions,
                       ) -> template.protocol.GetPositions:
         miner_hotkey = synapse.dendrite.hotkey
         if self.should_fail_early(miner_hotkey, synapse, SynapseMethod.POSITION_INSPECTOR):
--- template/protocol.py
+++ template/protocol.py
@@ -11,6 +11,21 @@ from pydantic import Field
 
 from typing import List
 
+
+original_Synapse_get_required_fields = bt.Synapse.get_required_fields
+
+# Monkey patch to improve Bittensor Synapse performance
+def synapse_get_required_fields(self):
+    _REQUIRED_FIELDS_ATTR = "__required_fields"
+    required_fields = getattr(self.__class__, _REQUIRED_FIELDS_ATTR, None)
+    if required_fields is None:
+        required_fields = original_Synapse_get_required_fields(self)
+        setattr(self.__class__, _REQUIRED_FIELDS_ATTR, required_fields)
+    return required_fields
+
+bt.Synapse.get_required_fields = synapse_get_required_fields
+
+
 class SendSignal(bt.Synapse):
     signal: typing.Dict = Field(default_factory=dict, title="Signal", frozen=False, max_length=4096)
     repo_version: str = Field("N/A", title="Repo version (use the same meta.json file as validator)", frozen=False, max_length=256)
