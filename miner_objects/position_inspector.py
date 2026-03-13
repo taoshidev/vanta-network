0e3728c6364c79ff18ec0a38e063aac00952c3f3
brokespace <mybrokenspacee@gmail.com>
2026-03-13 13:53:41 -0600

    fixed check

--- miner_objects/position_inspector.py
+++ miner_objects/position_inspector.py
@@ -25,7 +25,7 @@ class PositionInspector:
         self.recently_acked_validators = []
         self.stop_requested = False  # Flag to control the loop
         assert self.config.netuid in (8, 116), "Taoshi runs on netuid 8 (mainnet), 116 (testnet)"
-        self.is_testnet = self.config.netuid in (116)
+        self.is_testnet = self.config.netuid == 116
 
 
     async def run_update_loop(self):
