aa25367fc2922ed2296f3a6d727cc3301fadc215
ward-taoshi <236940013+ward-taoshi@users.noreply.github.com>
2026-05-13 18:22:42 -0400

    create FLAT ONLY trade pair tracking

--- neurons/validator.py
+++ neurons/validator.py
@@ -9,6 +9,7 @@ import threading
 import signal
 
 from vali_objects.enums.misc import SynapseMethod
+from vali_objects.enums.order_type_enum import OrderType
 from vanta_api.validator_api_manager import ValidatorAPIManager
 from shared_objects.rpc.server_orchestrator import ServerOrchestrator, NeuronContext
 from entity_management.entity_utils import is_synthetic_hotkey
@@ -555,8 +556,12 @@ class Validator(ValidatorBase):
             else:
                 is_market_open = self.price_fetcher_client.is_market_open(tp, now_ms)
                 execution_type = ExecutionType.from_string(signal.get("execution_type", "MARKET").upper())
+                order_type = OrderType.from_string(signal["order_type"])
                 if execution_type in [ExecutionType.MARKET, ExecutionType.FLAT_ALL] and not is_market_open:
                     synapse.error_message = f"Market for trade pair [{tp.trade_pair_id}] is closed. Please try again later."
+                    synapse.should_retry = False
+                elif tp.trade_pair_id in ValiConfig.FLAT_ONLY_TRADE_PAIR_IDS and order_type != OrderType.FLAT:
+                    synapse.error_message = (f"Trade pair [{tp.trade_pair_id}] is being discontinued. Please close your position.")
                     synapse.should_retry = False
                 else:
                     unsupported_check_start = time.perf_counter()
--- vali_objects/vali_config.py
+++ vali_objects/vali_config.py
@@ -660,6 +660,7 @@ class ValiConfig:
     # 100% percent of collateral deposit is at risk of slashing based on drawdown
     DRAWDOWN_SLASH_PROPORTION = 1.0
 
+    FLAT_ONLY_TRADE_PAIR_IDS = {'BCHUSD'}
     BLOCKED_TRADE_PAIR_IDS = {
         'SPX', 'DJI', 'NDX', 'VIX', 'FTSE', 'GDAXI',  # Indices
         'USDMXN'
