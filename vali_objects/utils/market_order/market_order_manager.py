0a5124d83b25e2c3919131bbdab4547ea4c3f623
sli-tao <sli@taoshi.io>
2026-09-17 23:21:55 -0700

    skip redundant price source fetch

--- vali_objects/utils/market_order/market_order_manager.py
+++ vali_objects/utils/market_order/market_order_manager.py
@@ -103,13 +103,13 @@ class MarketOrderManager():
             position = self._position_client.get_open_position_for_trade_pair(hotkey, trade_pair.trade_pair_id)
             position_type = position.position_type if position else order_type
 
-            if fill_price is None or not price_sources:
+            if not price_sources:
                 price_sources = self._live_price_client.get_sorted_price_sources_for_trade_pair(trade_pair, now_ms)
                 if not price_sources:
                     raise SignalException(f"Order Rejected: no live prices being found for {trade_pair.trade_pair_id}. Please try again.")
 
-                if fill_price is None:
-                    fill_price = price_sources[0].parse_appropriate_price(now_ms, trade_pair.is_forex, order_type, position_type)
+            if fill_price is None:
+                fill_price = price_sources[0].parse_appropriate_price(now_ms, trade_pair.is_forex, order_type, position_type)
 
             usd_base_rate = self._live_price_client.get_usd_base_conversion(trade_pair, now_ms, fill_price, order_type, position_type)
             quote_usd_rate = self._live_price_client.get_quote_usd_conversion(trade_pair, now_ms, fill_price, order_type, position_type)
